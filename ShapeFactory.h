f177121128f425d1ede51d96b7aa1391087da4a9
rafae <rafae@192.168.1.127>
2022-09-05 21:39:48 +0200

    Factory Method implementado para Shapes y sus subclases.

--- ShapeFactory.h
+++ ShapeFactory.h
@@ -1,8 +1,38 @@
 #ifndef SHAPEFACTORY_H_
 #define SHAPEFACTORY_H_
 
+#include "Shape.h"
 
 
+class ShapeFactory
+{
+public:
+	virtual Shape* createShape(Point iniciop, Point finalp, Color colorFrente, Color colorFondo) = 0;
+};
+
+
+class RectanguloFactory: public ShapeFactory
+{
+public:
+	RectanguloFactory() {}
+	Shape* createShape(Point iniciop, Point finalp, Color colorFrente, Color colorFondo);
+};
+
+
+class CirculoFactory: public ShapeFactory
+{
+public:
+	CirculoFactory() {}
+	Shape* createShape(Point iniciop, Point finalp, Color colorFrente, Color colorFondo);
+};
+
+
+class CuadradoFactory: public ShapeFactory
+{
+public:
+	CuadradoFactory() {}
+	Shape* createShape(Point iniciop, Point finalp, Color colorFrente, Color colorFondo);
+};
 
 
 #endif /* SHAPEFACTORY_H_ */
